5d5c7fd4a1a7f6982e251bc105358ac80f9468c1
rfhtorres28 <rfhtorres28@gmail.com>
2024-03-07 23:04:25 +0800

    Change the structure of the Quiz Web App

---
+++ ECEbank.py
@@ -0,0 +1,38 @@
+ece_questions = [
+{
+    'id': 1, 
+    'content': 'What is the meaning of B in BJT transistor?',
+    'options': [
+            {'question_id':1, 'letter': 'a', 'content': 'Bipolar', 'is_correct':True},
+            {'question_id':1, 'letter': 'b', 'content': 'Bisexual', 'is_correct':False},
+            {'question_id':1, 'letter': 'c', 'content': 'Bi-Current', 'is_correct':False}
+
+    ]
+
+},
+
+{
+    'id': 2, 
+    'content': 'What is the meaning of J in BJT transistor?',
+    'options': [
+            {'question_id':2, 'letter': 'a', 'content': 'Junction', 'is_correct':True},
+            {'question_id':2, 'letter': 'b', 'content': 'Jeje', 'is_correct':False},
+            {'question_id':2, 'letter': 'c', 'content': 'Jayson', 'is_correct':False}
+
+    ]
+
+},
+
+{
+    'id': 3, 
+    'content': 'What is the meaning of T in BJT transistor?',
+    'options': [
+            {'question_id':3,'letter': 'a', 'content': 'Tick', 'is_correct':False},
+            {'question_id':3, 'letter': 'b', 'content': 'Transistor', 'is_correct': True},
+            {'question_id':3, 'letter': 'c', 'content': 'Tate', 'is_correct': False}
+
+    ]
+
+}
+
+]
--- quiz.py
+++ quiz.py
@@ -1,46 +1,44 @@
-from flask import Flask, render_template, request, jsonify, make_response
-from flask_restful import Api, Resource
-from questionbank import questions
+from flask import Flask, request, render_template, url_for
+from ECEbank import ece_questions
 
 
 
 
 app = Flask(__name__)
-api = Api(app)
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('home.html')
 
 
 
-class SpecificQuestion(Resource):
-    def get(self, question_id=None):    
-        if question_id is None:
-            return questions 
-        response = {'message': 'No question found'}
-        return jsonify(questions[question_id-1]) if question_id >=1 and question_id <= len(questions) else make_response(jsonify(response), 404)
+@app.route('/electronics', methods=['GET', 'POST'])
+def elecs():
+     
+     correct_answer = 0
+     correct_letters = []
+     total_questions = len(ece_questions)
+      
 
-
-
-class Results(Resource):
-    def post(self):
-        correct_answer = 0
-        lis = {}
-        for question in questions: # loop through the list of dictionary questions
-             user_response = request.form.get(f'q{question["id"]}') # return the value pair, in this case, the letter choice of the user
-             correct_response = [x for x in question['options'] if x['is_correct']==True] # this return the correct answer for each question
-
-             if user_response == correct_response[0]["letter"]:
-                  correct_answer += 1
-
-        return jsonify(correct_answer)
-
-
-api.add_resource(SpecificQuestion, '/questions', '/questions/<int:question_id>')
-api.add_resource(Results, '/submit')
-
-
-
-@app.route('/')
-def quiz():
-    return render_template('quiz.html', questions=questions)
+     if request.method == 'GET': 
+         return render_template('quiz.html', questions=ece_questions)
+     
+     elif request.method == 'POST':
+        for question in ece_questions: # loop through the list of dictionary questions
+            user_response = request.form.get(f'q{question["id"]}') # return the value pair, in this case, the letter choice of the user
+            correct_response = [x for x in question['options'] if x['is_correct']==True] # this return the correct answer for each question
+            correct_letters.append(correct_response) # or correct_letters.append(correct_response[0] if correct_response else None)
+       
+            if user_response == correct_response[0]["letter"]:
+                correct_answer += 1
+        
+        score_percentage = correct_answer/total_questions*100
+        score_percentage = round(score_percentage, 2)
+        return render_template('result.html', correct_letters=correct_letters, 
+        correct_answer=correct_answer, total_questions=total_questions, score_percentage=score_percentage)
+         
+     
+        
 
 
 
