5970c21eee355d9e985243380a9dca1bbe1a2740
Ubuntu <ubuntu@ip-172-31-46-73.ap-southeast-1.compute.internal>
2024-06-05 12:58:01 +0000

    Modified run.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app import app
 from app import apimodels, config, forms, models, routes, utils
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
 
